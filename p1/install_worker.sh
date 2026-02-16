766edf89f2a801aa72b50740fd43fa8879df2b45
Loricko9 <lesaulnierlorick@gmail.com>
2026-02-16 16:22:16 +0100

    add p1 lorick

--- p1/install_worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt update && sudo apt install -y curl
-
-TOKEN=$(cat /vagrant/k3s_token.txt)
-
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${TOKEN} INSTALL_K3S_EXEC="--node-name aaudeberSW --node-ip 192.168.56.111" sh -
