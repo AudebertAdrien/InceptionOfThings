a554cd20ef17136d42ea58de140da645ceb3fa4c
Loricko9 <lesaulnierlorick@gmail.com>
2026-02-18 10:39:50 +0100

    upgrade RAM size for fix err

--- p2/Server.sh
+++ p2/Server.sh
@@ -5,4 +5,7 @@ sudo cp /vagrant/	config.yaml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | sh -
 sleep 5
 sudo k3s kubectl get nodes
+sleep 10
 sudo kubectl apply -f /vagrant/deployment.yaml
+sleep 20
+sudo kubectl get all
