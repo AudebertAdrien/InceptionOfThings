fe96802840a6ced47076fbb7cdb158d49920594c
Adrien Audebert <adrien.adbrt@tutanota.com>
2026-03-12 14:52:36 +0100

    p3

--- p1/scripts/install_worker.sh
+++ p1/scripts/install_worker.sh
@@ -7,6 +7,7 @@ while [ ! -f /share/k3s_token.txt ]; do
   echo "Waiting for master token in /share..."
   sleep 2
 done
+
 TOKEN=$(cat /share/k3s_token.txt)
 
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=${TOKEN} INSTALL_K3S_EXEC="--node-name aaudeberSW --node-ip 192.168.56.111" sh -
---
+++ p3/scripts/provisioning.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Install Docker
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker vagrant
+
+# Install kubectl
+if [ ! -f /usr/local/bin/kubectl ]; then
+  curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+fi
+
+# Install K3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+if ! k3d cluster list | grep -q "dev-cluster"; then
+  k3d cluster create dev-cluster --port 8888:8888@loadbalancer --wait
+fi
+
+mkdir -p /home/vagrant/.kube
+k3d kubeconfig get dev-cluster >/home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+chmod 600 /home/vagrant/.kube/config
+
+if ! grep -q "KUBECONFIG" /home/vagrant/.bashrc; then
+  echo 'export KUBECONFIG=/home/vagrant/.kube/config' >>/home/vagrant/.bashrc
+fi
+
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace dev --dry-run=client -o yaml | kubectl apply -f -
+
+# Install Argo CD
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Waiting for Argo CD pods to be ready..."
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
