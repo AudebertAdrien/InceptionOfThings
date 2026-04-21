9afbcbfb2b6ed7c64cd2651af40c82b80a64b240
Adrien Audebert <adrien.adbrt@tutanota.com>
2026-04-21 12:41:42 +0200

    bonne version de p3

--- p3/scripts/bootstrap.sh
+++ p3/scripts/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 K3D_CLUSTER_NAME="iot-cluster"
 USER_HOME="/home/vagrant"
@@ -7,7 +8,7 @@ echo "=================================================="
 echo "         System Setup & Docker Install            "
 echo "=================================================="
 
-sudo apt update && sudo apt install -y curl ca-certificates git
+sudo apt update && sudo apt install -y curl ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -22,6 +23,7 @@ EOF
 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo usermod -aG docker vagrant
 
 echo "=================================================="
@@ -36,6 +38,10 @@ fi
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+if ! k3d cluster list | grep -q "$K3D_CLUSTER_NAME"; then
+  k3d cluster create "$K3D_CLUSTER_NAME" --port "8080:80@loadbalancer" --wait
+fi
+
 mkdir -p "$USER_HOME/.kube"
 k3d kubeconfig get "$K3D_CLUSTER_NAME" >"$USER_HOME/.kube/config"
 chown -R vagrant:vagrant "$USER_HOME/.kube"
@@ -43,10 +49,6 @@ chmod 600 "$USER_HOME/.kube/config"
 
 if ! grep -q "KUBECONFIG" /home/vagrant/.bashrc; then
   echo 'export KUBECONFIG=/home/vagrant/.kube/config' >>/home/vagrant/.bashrc
-fi
-
-if ! k3d cluster list | grep -q "$K3D_CLUSTER_NAME"; then
-  k3d cluster create $K3D_CLUSTER_NAME --config /share/confs/config_k3d.yaml
 fi
 
 echo "=================================================="
@@ -57,19 +59,23 @@ kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace dev --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --server-side
-kubectl wait --for=condition=ready pods --all -n argocd --timeout=300s
-kubectl apply -f /share/confs/config_cluster.yaml
-kubectl rollout restart deployment argocd-server -n argocd
-kubectl rollout status deployment/argocd-server -n argocd --timeout=60s
-kubectl get all -n argocd
 
-kubectl -n argocd apply -f /share/confs/config_argocd.yaml
+echo "Waiting for Argo CD pods to be ready..."
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
+
+kubectl apply -f /share/confs/platform/argocd-params-cm.yaml
+kubectl apply -f /share/confs/platform/argocd-ing.yaml
+
+kubectl rollout restart deployment argocd-server -n argocd
+kubectl rollout status deployment argocd-server -n argocd --timeout=60s
+
+kubectl apply -f /share/confs/platform/argocd-app.yaml
 
 echo "=================================================="
 echo "             Installation Complete                "
 echo "=================================================="
 
 echo -n "Argo CD 'admin' Password: "
-kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo
 
